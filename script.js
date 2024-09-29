e79b916895e3d3411b1817dfd6018f93696283d1
Parag Vekariya <154518531+paragvekariya@users.noreply.github.com>
2024-09-29 21:33:40 +0530

    New script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+/*document.addEventListener("DOMContentLoaded", function() {
+  var typed = new Typed('#domains', {
+    strings: [' ', 'Software Engineer.', 'Python Developer.', 'Student..'],
+    typeSpeed: 40,
+    backspeed: 50,
+    loop: true
+  });
+});
+*/
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+        
